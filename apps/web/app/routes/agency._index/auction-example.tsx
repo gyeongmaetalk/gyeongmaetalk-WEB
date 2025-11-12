e62038c07e096a19fffb2f5f0d7568aaf8cd3841
gs0428 <lider28@naver.com>
2025-11-12 15:41:47 +0900

    fix: 이미지 교체 및 이미지 별 설명 수정

--- apps/web/app/routes/agency._index/auction-example.tsx
+++ apps/web/app/routes/agency._index/auction-example.tsx
@@ -4,8 +4,28 @@ import { Carousel, type CarouselApi, CarouselContent, CarouselItem } from "@gyeo
 
 import Autoplay from "embla-carousel-autoplay";
 
-import auctionExampleImaged from "~/assets/auction-example.png";
+import auctionExampleImage1 from "~/assets/auction-example1.webp";
+import auctionExampleImage2 from "~/assets/auction-example2.webp";
 import Image from "~/components/image";
+
+const getAuctionExampleTitle = (currentIndex: number) => {
+  if (currentIndex === 0) {
+    return (
+      <p className="font-heading2-bold text-label-strong">
+        경매톡에서 경매대행 시
+        <br />
+        <span className="text-primary-normal">무료로 추천매물</span>을 받아 볼 수 있어요!
+      </p>
+    );
+  }
+  return (
+    <p className="font-heading2-bold text-label-strong">
+      나에게 딱 맞는 <span className="text-primary-normal">추천매물</span>로
+      <br />
+      성공적인 경매를 진행해보세요
+    </p>
+  );
+};
 
 export default function AuctionExample() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -20,23 +40,26 @@ export default function AuctionExample() {
   }, [api]);
 
   return (
-    <Carousel
-      className="mb-2 mt-4"
-      opts={{ loop: true }}
-      setApi={setApi}
-      plugins={[Autoplay({ delay: 3000 })]}
-    >
-      <CarouselContent>
-        <CarouselItem>
-          <Image src={auctionExampleImaged} alt="경매대행 예시 이미지" />
-        </CarouselItem>
-        <CarouselItem>
-          <Image src={auctionExampleImaged} alt="경매대행 예시 이미지" />
-        </CarouselItem>
-      </CarouselContent>
-      <p className="bg-label-neutral font-caption1-regular absolute bottom-2.5 left-1/2 mx-auto -translate-x-1/2 rounded-full px-3 py-1 text-white">
-        {currentIndex + 1} / 2
-      </p>
-    </Carousel>
+    <>
+      {getAuctionExampleTitle(currentIndex)}
+      <Carousel
+        className="mt-4 mb-2"
+        opts={{ loop: true }}
+        setApi={setApi}
+        plugins={[Autoplay({ delay: 3000 })]}
+      >
+        <CarouselContent>
+          <CarouselItem>
+            <Image src={auctionExampleImage1} alt="경매대행 예시 이미지" />
+          </CarouselItem>
+          <CarouselItem>
+            <Image src={auctionExampleImage2} alt="경매대행 예시 이미지" />
+          </CarouselItem>
+        </CarouselContent>
+        <p className="bg-label-neutral font-caption1-regular absolute bottom-2.5 left-1/2 mx-auto -translate-x-1/2 rounded-full px-3 py-1 text-white">
+          {currentIndex + 1} / 2
+        </p>
+      </Carousel>
+    </>
   );
 }
