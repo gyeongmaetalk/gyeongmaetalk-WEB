4d18f9bcea3c2b2f0998bccd90cfb96b72f1249b
gs0428 <lider28@naver.com>
2026-01-22 01:09:54 +0900

    fix: 전역상태 초기화 로직 추가

--- apps/web/app/routes/mypage.userinfo/delete-user-modal.tsx
+++ apps/web/app/routes/mypage.userinfo/delete-user-modal.tsx
@@ -4,6 +4,7 @@ import { Button, Modal } from "@gyeongmaetalk/ui";
 import { useNavigate } from "react-router";
 
 import { useDeleteUser } from "~/lib/tanstack/mutation/auth";
+import { useUserStore } from "~/lib/zustand/user";
 import { errorToast } from "~/utils/toast";
 import { successToast } from "~/utils/toast";
 
@@ -14,10 +15,12 @@ interface DeleteUserModalProps {
 
 export default function DeleteUserModal({ isOpen, onClose }: DeleteUserModalProps) {
   const navigate = useNavigate();
+  const reset = useUserStore((state) => state.reset);
 
   const { mutateAsync: deleteUser, isPending: isDeleteUserPending } = useDeleteUser({
     onSuccess: () => {
       queryClient.resetQueries();
+      reset();
       successToast("회원탈퇴가 완료되었어요.");
       navigate("/", { replace: true });
     },
@@ -34,7 +37,7 @@ export default function DeleteUserModal({ isOpen, onClose }: DeleteUserModalProp
   return (
     <Modal className="max-w-xl">
       <Modal.Header>정말 회원탈퇴 하시겠습니까?</Modal.Header>
-      <Modal.Content className="text-left">
+      <Modal.Content>
         <p>회원탈퇴 시 모든 데이터가 삭제됩니다.</p>
       </Modal.Content>
       <Modal.Footer className="flex flex-col gap-2">
