2396eef352bf059b25841d64c121db6504f92689
gs0428 <lider28@naver.com>
2026-03-14 21:41:27 +0900

    chore: mixpanel 옵션 수정

--- apps/web/app/components/layout/mixpanel-provider.tsx
+++ apps/web/app/components/layout/mixpanel-provider.tsx
@@ -1,8 +1,12 @@
 import mixpanel from "mixpanel-browser";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 mixpanel.init("c32b81de65f6bb334a6cb411325572d2", {
-  autocapture: true,
+  debug: !isProduction,
   record_sessions_percent: 100,
+  track_pageview: true,
+  persistence: "localStorage",
 });
 
 export default function MixpanelProvider() {
