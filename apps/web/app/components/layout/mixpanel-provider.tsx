ada7ff5651f7bf3fb33960472981bf19001cf81d
gs0428 <lider28@naver.com>
2026-05-29 22:52:23 +0900

    chore: production 일때만 mixpanel init 되도록 수정

--- apps/web/app/components/layout/mixpanel-provider.tsx
+++ apps/web/app/components/layout/mixpanel-provider.tsx
@@ -4,12 +4,13 @@ import { ENV } from "~/constants/env";
 
 const isProduction = ENV.IS_PROD;
 
-mixpanel.init("c32b81de65f6bb334a6cb411325572d2", {
-  debug: !isProduction,
-  record_sessions_percent: 100,
-  track_pageview: true,
-  persistence: "localStorage",
-});
+if (isProduction) {
+  mixpanel.init("c32b81de65f6bb334a6cb411325572d2", {
+    record_sessions_percent: 100,
+    track_pageview: true,
+    persistence: "localStorage",
+  });
+}
 
 export default function MixpanelProvider() {
   return null;
